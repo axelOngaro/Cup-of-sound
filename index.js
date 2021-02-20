0a69f5ebda76af350a1e5db69496d25de6c56fd3
Axel <axel.ongaro.pro@gmail.com>
2021-02-20 16:56:31 +0100

    Tried to add scroll event

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-import LocomotiveScroll from 'locomotive-scroll';
+import LocomotiveScroll from "locomotive-scroll";
 
 const scroll = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
+  el: document.querySelector("[data-scroll-container]"),
   smooth: true,
 });
+const target = document.querySelector("#about-page");
+
+const scrollAbout = (scroll, target) => {
+  console.log("scrollmdr");
+  scroll.scrollTo(target);
+};
+
+export default scrollAbout;
